4abb0e3515aaee420b587baca6b22189cd936136
zxand1 <zxandnaxx@gmail.com>
2024-11-27 18:35:56 -0300

    Start APi

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,39 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export interface Env {
+  SPACE: KVNamespace;
+}
+
+interface VagaData {
+	vaga: number;
+  placa: string;
+  foto: string;
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
+  async fetch(request, env, ctx): Promise<Response> {
+    try {
+      const url = new URL(request.url);
+      const vagaId = url.pathname.split('/').pop();
+      if (!vagaId) {
+        return new Response("Vaga ID is required", { status: 400 });
+      }
+      const body : VagaData = await request.json();
+      const valueToStore = {
+        vaga: body.vaga,
+        placa: body.placa,
+        foto: body.foto
+      };
+      await env.SPACE.put(vagaId, JSON.stringify(valueToStore));
+      const value = await env.SPACE.get(vagaId);
+      if (value === null) {
+        return new Response("Vaga not found", { status: 404 });
+      }
+      const parsedValue = JSON.parse(value);
+      return new Response(JSON.stringify(parsedValue), {
+        headers: { "Content-Type": "application/json" }
+      });
+    } catch (err) {
+      console.error(`KV returned error: ${err}`);
+      return new Response(err as string, { status: 500 });
+    }
+  },
 } satisfies ExportedHandler<Env>;
